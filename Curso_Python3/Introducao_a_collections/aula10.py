398c67eac56e19b1435cab3ec97db2bb89622fdd
vladimirmaciel <vladimirmaciel@gmail.com>
2021-12-28 20:00:12 -0300

    finalizando collection

---
+++ Curso_Python3/Introducao_a_collections/aula10.py
@@ -0,0 +1,5 @@
+idades = [15, 87, 32, 65, 56, 32, 49, 37]
+
+print(sorted(idades))
+print(sorted(idades, reverse=True))
+print(list(reversed(idades)))  # esta pegando a maior posição não o maior valor
